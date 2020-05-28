8f8293ee92a7b9fedf4e6bf117673ef9d43474b3
Esteban Fuentealba <efuentealba@json.cl>
2020-05-28 00:58:50 -0400

    Added: example read & write ROM

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ const rom = new Rom({
 const { teams } = rom.read();
 
 //  update player info
+//  Edit goalkeeper name of Cruzeiro
+console.log("before", teams[0].getPlayers()[0].getName());
 teams[0].getPlayers()[0].setName("Coronavi");
-
+console.log("after", teams[0].getPlayers()[0].getName());
 //  write ROM
 rom.write(`${__dirname}/../out/fecic.1_EDITED.smc`);
 
